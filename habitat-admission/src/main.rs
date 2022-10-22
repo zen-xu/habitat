3b825697b4e3e6aaa57f8715a685040ad229ba3f
ZhengYu, Xu <zen-xu@outlook.com>
2022-10-22 18:58:05 +0800

    feat(admission): enable cli options

--- habitat-admission/src/main.rs
+++ habitat-admission/src/main.rs
@@ -1,34 +1,59 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use tracing::*;
 use warp::Filter;
 
 mod mutate;
 mod validate;
 
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, help = "Server addr", default_value = "0.0.0.0")]
+    ip_addr: std::net::IpAddr,
+
+    #[arg(long, help = "Server port", default_value_t = 8443)]
+    port: u32,
+
+    #[arg(long, required = true, help = "Specify the file path to read the certificate")]
+    cert_path: PathBuf,
+
+    #[arg(long, required = true, help = "Specify the file path to read the private key")]
+    key_path: PathBuf,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    // You must generate a certificate for the service / url,
-    // encode the CA in the MutatingWebhookConfiguration, and terminate TLS here.
-    // See admission_setup.sh + admission_controller.yaml.tpl for how to do this.
-    let addr = format!("{}:8443", std::env::var("ADMISSION_PRIVATE_IP").unwrap());
+    let args = Args::parse();
+    run(args.ip_addr, args.port, args.cert_path, args.key_path).await;
+}
 
-    let mutate = warp::path("mutate")
+async fn run(ip_addr: std::net::IpAddr, port: u32, cert_path: PathBuf, key_path: PathBuf) {
+    let addr = format!("{}:{}", ip_addr, port);
+
+    let healthy = warp::get().and(warp::path("health")).map(|| "healthy");
+
+    let mutate = warp::post()
+        .and(warp::path("mutate"))
         .and(warp::body::json())
         .and_then(crate::mutate::handler)
         .with(warp::trace::request());
 
-    let validate = warp::path("validate")
+    let validate = warp::post()
+        .and(warp::path("validate"))
         .and(warp::body::json())
         .and_then(crate::validate::handler)
         .with(warp::trace::request());
 
     info!("starting habitat admission controller");
-    warp::serve(warp::post().and(mutate.or(validate)))
+    warp::serve(healthy.or(mutate).or(validate))
         .tls()
-        .cert_path("habitat-admission/caches/admission-controller-tls.crt")
-        .key_path("habitat-admission/caches/admission-controller-tls.key")
-        // .run(([0, 0, 0, 0], 8443)) // in-cluster
-        .run(addr.parse::<std::net::SocketAddr>().unwrap()) // local-dev
+        .cert_path(cert_path)
+        .key_path(key_path)
+        .run(addr.parse::<std::net::SocketAddr>().unwrap())
         .await;
 }
