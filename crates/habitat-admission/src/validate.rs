9ebaa647d129bf1dd13138d8d0a5a40e3cb51826
ZhengYu, Xu <zen-xu@outlook.com>
2022-11-23 16:29:21 +0800

    support admission to validate pod template (#37)

--- crates/habitat-admission/src/validate.rs
+++ crates/habitat-admission/src/validate.rs
@@ -1,7 +1,12 @@
-use habitat_api::Job;
-use kube::core::{
-    admission::{AdmissionRequest, AdmissionResponse, AdmissionReview},
-    DynamicObject, ResourceExt,
+use habitat_api::{batch::TaskSpec, Job};
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    core::{
+        admission::{AdmissionRequest, AdmissionResponse, AdmissionReview},
+        params::PostParams,
+        DynamicObject, ObjectMeta, ResourceExt,
+    },
+    Api, Client,
 };
 use std::{convert::Infallible, error::Error};
 use tracing::*;
@@ -20,6 +25,7 @@ pub async fn handler(body: AdmissionReview<DynamicObject>) -> Result<impl Reply,
             ));
         }
     };
+    let client = kube::Client::try_default().await.unwrap();
 
     // Then construct a AdmissionResponse
     let mut res = AdmissionResponse::from(&req);
@@ -27,7 +33,7 @@ pub async fn handler(body: AdmissionReview<DynamicObject>) -> Result<impl Reply,
     if let Some(obj) = req.object {
         let name = obj.name_any(); // apiserver may not have generated a name yet
         res = match try_cast_dynamic_obj_into_job(&obj) {
-            Ok(job) => match validate(res.clone(), &job) {
+            Ok(job) => match validate(res.clone(), &job, client).await {
                 Ok(res) => {
                     info!("accepted: {:?} on Job {}", req.operation, name);
                     res
@@ -48,13 +54,19 @@ pub async fn handler(body: AdmissionReview<DynamicObject>) -> Result<impl Reply,
 }
 
 // The main handler and core business logic, failures here implies rejected applies
-fn validate(res: AdmissionResponse, obj: &Job) -> Result<AdmissionResponse, Box<dyn Error>> {
+async fn validate(
+    res: AdmissionResponse,
+    obj: &Job,
+    client: Client,
+) -> Result<AdmissionResponse, Box<dyn Error>> {
     if obj.spec.tasks.is_empty() {
         return Err("no task specified".into());
     }
 
+    let pods: Api<Pod> = Api::namespaced(client, &obj.namespace().unwrap());
+
     // If the task parallelism.min > parallelism.max, we reject it.
-    for task in obj.spec.tasks.iter() {
+    for (idx, task) in obj.spec.tasks.iter().enumerate() {
         if task.parallelism.min > task.parallelism.max {
             return Err(format!(
                 "task `{}` parallelism.min can't greater than parallelism.max",
@@ -62,7 +74,54 @@ fn validate(res: AdmissionResponse, obj: &Job) -> Result<AdmissionResponse, Box<
             )
             .into());
         }
+
+        // create a template pod and validate it in the server side
+        let pod = new_template_pod(task);
+        if let Err(err) = pods
+            .create(
+                &PostParams {
+                    dry_run: true,
+                    field_manager: None,
+                },
+                &pod,
+            )
+            .await
+        {
+            return match err {
+                kube::Error::Api(err) => {
+                    let message = format!(
+                        "spec.tasks[{}].template.{}",
+                        idx,
+                        err.message
+                            .trim_start_matches(&format!("Pod \"{}\" is invalid: ", pod.name_any()))
+                    );
+                    Err(message.into())
+                }
+                _ => Err(err.into()),
+            };
+        }
     }
 
     Ok(res)
+}
+
+fn new_template_pod(task_spec: &TaskSpec) -> Pod {
+    Pod {
+        metadata: ObjectMeta {
+            name: Some(task_spec.name.clone()),
+            labels: task_spec
+                .template
+                .metadata
+                .as_ref()
+                .and_then(|m| m.labels.clone()),
+            annotations: task_spec
+                .template
+                .metadata
+                .as_ref()
+                .and_then(|m| m.annotations.clone()),
+            ..Default::default()
+        },
+        spec: Some(serde_json::from_str(&serde_json::to_string(&task_spec.template.spec).unwrap()).unwrap()),
+        ..Default::default()
+    }
 }
